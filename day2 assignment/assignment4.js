6a0cd85f73591b4c51ebc18c15aaf48c778358eb
jagrutipawar <68052046+jagrutipawar@users.noreply.github.com>
2020-08-31 13:42:29 +0530

    Add files via upload

---
+++ day2 assignment/assignment4.js
@@ -0,0 +1,16 @@
+let data=["Letsupgrade","Python","javascript","excel","website","assign"]
+for(let i=0;i<=5;i++)
+{
+    // if(data.search("a")==true)
+    // {
+    //     let ele=data.search();
+    //     console.log(ele);
+    // }
+    if(data[i].search("a"))
+    {
+        let ele=data[i];
+       document.write(data[i]);
+        document.write("<br>");
+        
+    }
+}
