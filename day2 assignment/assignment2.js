9759eb9e3db6555025e0329c3bc63860df985783
jagrutipawar <68052046+jagrutipawar@users.noreply.github.com>
2020-09-04 13:55:10 +0530

    Delete assignment2.js

--- day2 assignment/assignment2.js
+++
@@ -1,7 +0,0 @@
-function convertMinutestoSeconds(minutes) 
-    {
-      return Math.floor(minutes * 60);
-    }
-    var minutesToSeconds = convertMinutestoSeconds(2); // convert minutes to second javascript 
-    document.write( "Result of converting minutes to seconds :- " + minutesToSeconds ); 
- 
